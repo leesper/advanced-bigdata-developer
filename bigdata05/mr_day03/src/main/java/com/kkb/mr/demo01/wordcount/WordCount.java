0c96270afd63ab9661ae85372e23138c02a7e90e
leesper <pascal7718@gmail.com>
2020-01-25 16:58:16 +0800

    feat: Finish map reduce and main logic

--- bigdata05/mr_day03/src/main/java/com/kkb/mr/demo01/wordcount/WordCount.java
+++ bigdata05/mr_day03/src/main/java/com/kkb/mr/demo01/wordcount/WordCount.java
@@ -1,4 +1,52 @@
 package com.kkb.mr.demo01.wordcount;
 
-public class WordCount {
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WordCount extends Configured implements Tool {
+    @Override
+    public int run(String[] args) throws Exception {
+        Configuration conf = super.getConf();
+        Job job = Job.getInstance(conf, "WordCount");
+
+        // ready to submit to cluster
+        job.setJarByClass(WordCount.class);
+
+        // step1: InputFormat
+        job.setInputFormatClass(TextInputFormat.class);
+        TextInputFormat.addInputPath(job, new Path(args[0]));
+
+        // step2: map()
+        job.setMapperClass(WordCountMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        // step3, 4, 5, 6: ignored
+
+        // step7: reduce()
+        job.setReducerClass(WordCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // step8: OutputFormat
+        job.setOutputFormatClass(TextOutputFormat.class);
+        TextOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        boolean b = job.waitForCompletion(true);
+        return b ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration configuration = new Configuration();
+        int run = ToolRunner.run(configuration, new WordCount(), args);
+        System.exit(run);
+    }
 }
