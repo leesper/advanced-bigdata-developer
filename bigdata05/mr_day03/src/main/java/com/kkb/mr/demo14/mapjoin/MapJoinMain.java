749ef9bbcd87e5fef25510c65c8ed03880cc9d9d
leesper <pascal7718@gmail.com>
2020-02-04 11:13:45 +0800

    feat: Add examples for map join(distributed cache)

---
+++ bigdata05/mr_day03/src/main/java/com/kkb/mr/demo14/mapjoin/MapJoinMain.java
@@ -0,0 +1,48 @@
+package com.kkb.mr.demo14.mapjoin;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.net.URI;
+
+public class MapJoinMain extends Configured implements Tool {
+    @Override
+    public int run(String[] args) throws Exception {
+        URI uri = new URI("hdfs://node01:8020/cache/pdts.txt");
+        Configuration conf = super.getConf();
+        //添加文件到分布式缓存
+        DistributedCache.addCacheFile(uri,conf);
+        //获取job对象
+        Job job = Job.getInstance(conf, "mapJoin");
+        //读取文件，解析成为key，value对
+        job.setInputFormatClass(TextInputFormat.class);
+        TextInputFormat.addInputPath(job, new Path(args[0]));
+
+        job.setMapperClass(MapJoinMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(NullWritable.class);
+
+        //没有reducer逻辑，不用设置了
+        job.setOutputFormatClass(TextOutputFormat.class);
+        TextOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        boolean b = job.waitForCompletion(true);
+
+        return b ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int run = ToolRunner.run(new Configuration(), new MapJoinMain(), args);
+        System.exit(run);
+    }
+
+}
