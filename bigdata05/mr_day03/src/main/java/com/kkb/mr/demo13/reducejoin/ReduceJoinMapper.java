d45cd5ea212b9e0eb6a8b48b51003b4409cd4943
leesper <pascal7718@gmail.com>
2020-02-04 10:45:12 +0800

    feat: Add examples for reduce join

---
+++ bigdata05/mr_day03/src/main/java/com/kkb/mr/demo13/reducejoin/ReduceJoinMapper.java
@@ -0,0 +1,38 @@
+package com.kkb.mr.demo13.reducejoin;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class ReduceJoinMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+    //现在我们读取了两个文件，如何确定当前处理的这一行数据是来自哪一个文件里面的
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        // 方法1：通过文件名判断.获取文件的切片
+        // get the input file split
+//        FileSplit inputSplit = (FileSplit) context.getInputSplit();
+        // get the file name
+//        String name = inputSplit.getPath().getName();
+//        if (name.equals("orders.txt")) {
+            //订单表数据
+//        } else {
+            //商品表数据
+//        }
+
+        // 方法2：根据订单和商品数据的特殊性来判断
+        String[] split = value.toString().split(",");
+
+        if (value.toString().startsWith("p")) {
+            //原始数据：p0002,锤子T1,1000,3000
+            //以商品id作为key,相同商品的数据都会到一起去
+            context.write(new Text(split[0]), value);
+        } else {
+            //order
+            context.write(new Text(split[2]), value);
+        }
+    }
+}
