801ac9e92e34d4cf8c122b538c42261c914ae824
leesper <pascal7718@gmail.com>
2020-02-16 22:55:30 +0800

    feat: Add examples for JDBC accessing hive

---
+++ bigdata05/hive/src/main/java/com/kkb/hive/demo01/HiveJDBC.java
@@ -0,0 +1,25 @@
+package com.kkb.hive.demo01;
+
+import java.sql.*;
+
+public class HiveJDBC {
+    private static String url = "jdbc:hive2://node3:10000/";
+
+    public static void main(String[] args) throws Exception {
+        Class.forName("org.apache.hive.jdbc.HiveDriver");
+        Connection connection = DriverManager.getConnection(url, "hadoop", "");
+        String sql = "SELECT * FROM stu";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                System.out.println(id + "\t" + name);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
